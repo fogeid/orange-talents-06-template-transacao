d20999897ee5e9788657771703b383f01899a2fa
Diego Batista <diego.silva@aluno.ic.ufmt.br>
2021-08-02 16:36:37 -0400

    feat: implementação keycloak

---
+++ src/main/java/br/com/zupacademy/diego/transacao/config/security/SecurityConfigurations.java
@@ -0,0 +1,18 @@
+package br.com.zupacademy.diego.transacao.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests -> authorizeRequests
+                .antMatchers(HttpMethod.GET, "/compras/**").hasAuthority("SCOPE_transacao:read")
+                .anyRequest().authenticated()
+        ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
